93e6a5b653dab5a7a6289e7ffab81b85b9cea556
marvincd <61430224+marvincd@users.noreply.github.com>
2022-05-28 13:27:02 +0100

    login, signup and dashboard page
    code structure

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { EmployeeDashboardComponent } from './employee-dashboard/employee-dashboard.component';
+import { LoginComponent } from './login/login.component';
+import { AuthGuard } from './shared/auth.guard';
+import { SignupComponent } from './signup/signup.component';
+
+const routes: Routes = [
+  {path:'', redirectTo:'login',pathMatch:'full'},
+  {path:'login', component:LoginComponent},
+  {path:'dashboard', component:EmployeeDashboardComponent
+  // canActivate:[AuthGuard]
+},
+  {path:'signup', component: SignupComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
